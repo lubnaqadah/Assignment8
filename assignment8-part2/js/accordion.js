9344e28a30f7bf59a31326c99f9b6f00779a9d14
lubnaqadah <lubna.alqdah@gmail.com>
2016-08-03 10:47:59 -0700

    Update accordion.js

--- assignment8-part2/js/accordion.js
+++ assignment8-part2/js/accordion.js
@@ -1,8 +1,8 @@
 $(function(){
     $('#accordion .heading').click(function(){
-
-      $(this).next().slideToggle('fast');
-	  $(this).toggleClass('active');	
-     $(".cont").not($(this).next()).slideUp('fast');
+	$(".heading").not($(this)).removeClass("active");	
+	$(this).next().slideToggle('fast');
+	$(this).toggleClass('active');	
+	$(".cont").not($(this).next()).slideUp('fast');
 	});
 })
